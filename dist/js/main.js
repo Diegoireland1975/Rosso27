b97781eb66e76014767a990a3374e8dd37bdf1ee
unknown <diego170975@gmail.com>
2020-11-29 19:15:31 +0000

    fixed slider

---
+++ dist/js/main.js
@@ -0,0 +1,7 @@
+$('[data-fancybox="Michele"]').fancybox({
+  loop: true,
+  transitionEffect: "zoom-in-out",
+  animationDuration: 366,
+  transitionDuration: 1000,
+  buttons: ["zoom", "slideShow", "fullScreen", "thumbs", "close"],
+});
