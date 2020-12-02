f493b06707ad1b5b4244b6cdea4978c40bd066a4
unknown <diego170975@gmail.com>
2020-12-02 17:12:25 +0000

    added a new page

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,3 +5,11 @@ $('[data-fancybox="Michele"]').fancybox({
   transitionDuration: 1000,
   buttons: ["zoom", "slideShow", "fullScreen", "thumbs", "close"],
 });
+
+$('[data-fancybox="victories"]').fancybox({
+  loop: true,
+  transitionEffect: "zoom-in-out",
+  animationDuration: 366,
+  transitionDuration: 1000,
+  buttons: ["zoom", "slideShow", "fullScreen", "thumbs", "close"],
+});
