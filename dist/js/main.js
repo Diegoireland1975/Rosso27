f815b72855246d9c2d57d6c18934f92ceb834abb
Diego Raffa <diego170975@gmail.com>
2023-11-04 14:25:30 +0000

    implemented load more button in gallery

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,8 +16,27 @@ const mainHeader = document.getElementById("main-header");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 150) {
-    mainHeader.style.opacity = 0.95;
+    mainHeader.style.opacity = 0.96;
   } else {
     mainHeader.style.opacity = 1;
+  }
+});
+
+// ##################
+// LOAD MORE BUTTON
+// ###################
+const wrapper = document.querySelectorAll("#wrapper");
+const btnLoadMore = document.querySelector("#btn-load-more");
+let currentImg = 2;
+
+btnLoadMore.addEventListener("click", () => {
+  for (let i = currentImg; i < currentImg + 1; i++) {
+    if (wrapper[i]) {
+      wrapper[i].style.display = "inline-flex";
+    }
+  }
+  currentImg += 1;
+  if (currentImg >= wrapper.length) {
+    btnLoadMore.style.display = "none";
   }
 });
